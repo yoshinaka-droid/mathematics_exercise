05b1ca6fb0e358201aa6bf470525558a94edda39
yoshinaka-droid <yoshinaka012825@gmail.com>
2024-07-06 16:37:40 +0900

    first commit

---
+++ Analysis/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Algebra</h1>
+      <p>ローカルサーバの立ち上げ方は、ディレクトリを変えた後に'npm run dev'</p>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
