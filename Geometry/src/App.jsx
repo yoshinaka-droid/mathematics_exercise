437d26068eda4bfef185f4740866286df9d714ab
yoshinaka-droid <yoshinaka012825@gmail.com>
2024-07-09 22:01:51 +0900

    リンクを作った

--- Geometry/src/App.jsx
+++ Geometry/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Geometry</h1>
       <p>ローカルサーバの立ち上げ方は、ディレクトリを変えた後に'npm run dev'</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Algebra is {count}
-        </button>
-      </div>
+      <a href='./src/firstbuild/firstbuild.pdf'>最初のビルド</a>
     </>
   )
 }
